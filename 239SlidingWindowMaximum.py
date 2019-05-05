5288552c6edfb7025a38b390139159c494a8c545
Caoyuan Li <408271618@qq.com>
2019-05-05 12:24:26 +1000

    Update 239SlidingWindowMaximum.py

--- 239SlidingWindowMaximum.py
+++ 239SlidingWindowMaximum.py
@@ -1,3 +1,81 @@
+#I have totally forget how to solve it half year later, however, I saw and tried some more smarter solutions
+from Queue import PriorityQueue
+from collections import deque
+class Solution(object):
+    def findMax(self, pq, start):
+        while True:
+            value, index = pq.get()
+            if index>=start:
+                pq.put((value, index))
+                return (value,index)
+    def add_to_deq(self,dq, nums, index):
+        while len(dq) and nums[index]>=nums[dq[-1]]:
+            dq.pop()
+        dq.append(index)
+        
+    def maxSlidingWindow(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        #Soultion 3 much quicker use a pointer to point the max value in window
+        if not nums or k == 0: return []
+        reuslt = [0] * (len(nums) - k + 1)
+        last_max_index = -1
+        for i in range(len(nums) - k + 1):
+            if last_max_index < i:
+                last_max_index = i
+                for j in range(i, i + k):
+                    if nums[j] > nums[last_max_index]: last_max_index = j
+            elif nums[last_max_index] < nums[i + k - 1]: last_max_index = i + k - 1
+            reuslt[i] = nums[last_max_index]
+        return reuslt
+        
+        
+        #Solution 2 use dequeue
+        # if k==1:
+        #     return nums
+        # if len(nums)==0:
+        #     return []
+        # dq= deque()
+        # for i in range(k):
+        #     self.add_to_deq(dq, nums, i)
+        # result = []
+        # start = 0
+        # for i in range(k-1,len(nums)):
+        #     self.add_to_deq(dq, nums, i)
+        #     while dq[0]<start:
+        #         dq.popleft()
+        #     result.append(nums[dq[0]])
+        #     start+=1
+        # return result
+               
+        
+        #Solution 1, use PriorityQueue, find max value in a k-sized Priority Queue
+#         if len(nums)==0:
+#             return []
+#         pq = PriorityQueue()
+#         result = []
+#         if k==1:
+#             return nums
+#         start=0
+#         for i in range(k):
+#             pq.put((-nums[i],i))
+        
+#         for i in range(k-1,len(nums)):
+#             pq.put((-nums[i],i)) 
+#             value, index = self.findMax(pq, start)        
+#             result.append(-value)   
+#             start+=1
+#         return result
+            
+            
+        
+        
+        
+
+
 import collections
 import heapq as h
 #Provided three solutions:
