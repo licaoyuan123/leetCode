62366634d77895831fd9fcb02212067b89f44dde
Caoyuan Li <408271618@qq.com>
2019-05-12 22:19:36 +1000

    Update 098validateBinarySearchTree.py

--- 098validateBinarySearchTree.py
+++ 098validateBinarySearchTree.py
@@ -1,4 +1,52 @@
 # Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isValidBST(self, root, lo=-float('inf'), hi = float('inf')):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if not root:
+            return True
+        if lo<root.val<hi:
+            return self.isValidBST(root.left, lo, min(hi,root.val)) and self.isValidBST(root.right,max(root.val, lo), hi)
+       # return 
+        
+        #My initial solution, however it is out of time limition
+#         if root==None:
+#             return True
+#         if root.left!=None:
+#             if self.ma(root.left)>=root.val:
+#                 return False
+#         if root.right!=None:
+#             if root.val>=self.mi(root.right):
+#                 return False
+        
+#         return self.isValidBST(root.left) and self.isValidBST(root.right)
+#     def ma(self, root):
+#         max_value = root.val
+#         if root.left and self.ma(root.left)>max_value:
+#             max_value = self.ma(root.left)
+#         if root.right and self.ma(root.right)>max_value:
+#             max_value = self.ma(root.right)
+#         return max_value
+            
+#     def mi(self, root):
+#         min_value = root.val
+#         if root.left and self.mi(root.left)<min_value:
+#             min_value = self.mi(root.left)
+#         if root.right and self.mi(root.right)<min_value:
+#             min_value = self.mi(root.right )
+#         return min_value
+
+
+
+# Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
