0d0f9d7cb4f7d320cd2281a483554e177c0777a9
Caoyuan Li <408271618@qq.com>
2019-06-27 17:51:45 +1000

    Create 039CombinationSum.py

---
+++ 039CombinationSum.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        res = []
+        candidates.sort()
+        self.dfs(candidates, target,0, [], res)
+        return res
+        
+        
+    def dfs(self, nums, target,index, path, res):
+        if target<0:
+            return
+        if target==0:
+            res.append(path)
+            return
+        for i in xrange(index ,len(nums)):
+            self.dfs(nums, target- nums[i],i, path+[nums[i]], res )
+        
+        
+#         results=[]
+#         re= []
+#         for i in range(len(candidates)):
+#             if candidates[i]<target:
+                
+#             if candidates[i]==target:
+#                 results.append([target])
+#             if candidates[i]>target:
+#                 break
+#         return results
