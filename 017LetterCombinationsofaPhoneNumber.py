2f49adc181bd6e935f6e93bc3a762c1bf29bbfaf
Caoyuan Li <408271618@qq.com>
2019-07-11 23:05:27 +1000

    dfs and three loop

--- 017LetterCombinationsofaPhoneNumber.py
+++ 017LetterCombinationsofaPhoneNumber.py
@@ -24,4 +24,25 @@ class Solution(object):
             
         for j in range(len(ma[st[index]])):
             self.dfs(st, ma,index+1, path+ma[st[index]][j], res)
+          
+ class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        if not digits:
+            return []
+        
+        ma= {"2":["a", "b", "c"], "3":["d", "e", "f"], "4":["g", "h", "i"], "5":["j", "k", "l"], "6":["m", "n", "o"], "7":["p", "q", "r", "s"], "8":["t", "u", "v"], "9":["w", "x", "y", "z"]}
+        
+        all_comb = ['']
+        for digit in digits:
+            current_comb = list()
+            
+            for letter in ma[digit]:
+                for comb in all_comb:
+                    current_comb.append(comb+letter)
+            all_comb = current_comb
+        return all_comb
         
