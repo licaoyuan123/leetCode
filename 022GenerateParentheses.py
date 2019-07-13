2c06409fd560b4e4c192d90485c2469e93d23ff8
Caoyuan Li <408271618@qq.com>
2019-07-13 16:22:59 +1000

    dfs and DP

--- 022GenerateParentheses.py
+++ 022GenerateParentheses.py
@@ -4,16 +4,26 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
-        left, right = 0,0
-        S = ""
-        ans=[]
-        def dfs(S, left, right, ans):
-            if len(S)==2*n:
-                ans.append(S)
-                return
-            if left<n:
-                dfs(S+'(', left+1, right, ans)
-            if right<left:
-                dfs(S+")", left, right+1, ans)
-        dfs(S, left, right, ans)
-        return ans
+        dp=[[] for i in range(n+1)]
+        dp[0].append("")
+        for i in range(1, n+1):
+            #dp[i] = ['('+x+')' for x in dp[i-1] ] 
+            for j in range(i):
+                dp[i]+= ['('+ x +')'+y for y in dp[j] for x in dp[i-j-1] ] 
+        return dp[n]
+        #dp[0] = 
+        
+        
+        # left, right = 0,0
+        # S = ""
+        # ans=[]
+        # def dfs(S, left, right, ans):
+        #     if len(S)==2*n:
+        #         ans.append(S)
+        #         return
+        #     if left<n:
+        #         dfs(S+'(', left+1, right, ans)
+        #     if right<left:
+        #         dfs(S+")", left, right+1, ans)
+        # dfs(S, left, right, ans)
+        # return ans
