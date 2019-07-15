296ad9d44d8add4c078c67c095d9f5e73fa1da2e
Caoyuan Li <408271618@qq.com>
2019-07-15 21:43:43 +1000

    dict collections.defaultdict

---
+++ 049GroupAnagrams.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        ans  =collections.defaultdict(list)
+        for s in strs:
+            ans[tuple(sorted(s))].append(s)
+        return ans.values()
+        
+        
+        # ans= collections.defaultdict(list)
+        # #ans= {}
+        # for s in strs:
+        #     count = [0]*26
+        #     for c in s:
+        #         count[ord(c)- ord('a')]+=1
+        #     ans[tuple(count)].append(s)
+        # return ans.values()
+        
