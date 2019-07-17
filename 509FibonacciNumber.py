69367aba836af781bdf8763e3b68fcc1c81514f8
Caoyuan Li <408271618@qq.com>
2019-07-17 22:18:02 +1000

    DP and Iterative

--- 509FibonacciNumber.py
+++ 509FibonacciNumber.py
@@ -4,15 +4,26 @@ class Solution(object):
         :type N: int
         :rtype: int
         """
+        a=0
+        b=1
+        for _ in range(N):
+            a, b= b, a+b
+        return a
+        # count = 0
+        # while count<=N:
+        #     c=a+b
+        #     a=b
+        #     b=c
+        #     count+=1
+        # return c
         
+#         dp=[0]*(N+2)
+#         dp[1]=1
+#         if N<2:
+#             return dp[N]
         
-        dp=[0]*(N+2)
-        dp[1]=1
-        if N<2:
-            return dp[N]
-        
-        for i in range(2, N+1):
-            dp[i]+=dp[i-1]
-            if i!=1:
-                dp[i]+=dp[i-2]
-        return dp[N]
+#         for i in range(2, N+1):
+#             dp[i]+=dp[i-1]
+#             if i!=1:
+#                 dp[i]+=dp[i-2]
+#         return dp[N]
