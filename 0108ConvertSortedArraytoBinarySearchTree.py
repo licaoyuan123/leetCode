81201fe0516051b5012b176be53375bbf7f7a05a
Caoyuan Li <408271618@qq.com>
2019-08-01 21:30:39 +1000

    recursive

--- 0108ConvertSortedArraytoBinarySearchTree.py
+++ 0108ConvertSortedArraytoBinarySearchTree.py
@@ -11,11 +11,26 @@ class Solution(object):
         :type nums: List[int]
         :rtype: TreeNode
         """
-        if not nums:
-            return None
-        mid  =len(nums)//2
-        root = TreeNode(nums[mid])
-        root.left = self.sortedArrayToBST(nums[0:mid])
-        root.right = self.sortedArrayToBST(nums[mid+1:])
-        return root
+#         if not nums:
+#             return None
+        
+        def convert(left, right):
+            if left>right:
+                return None
+            mid= (left+right)//2
+            root = TreeNode(nums[mid])
+            root.left = convert(left, mid-1)
+            root.right = convert(mid+1, right)
+            return root
+        return convert(0, len(nums)-1)
+                
+        
+        
+        # if not nums:
+        #     return None
+        # mid  =len(nums)//2
+        # root = TreeNode(nums[mid])
+        # root.left = self.sortedArrayToBST(nums[0:mid])
+        # root.right = self.sortedArrayToBST(nums[mid+1:])
+        # return root
         
