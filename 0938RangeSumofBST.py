99b1d97c176a4093f890ef2508359a29e9e5c4a1
Caoyuan Li <408271618@qq.com>
2019-08-15 21:35:10 +1000

    recursive, iterative, stack, BST

--- 0938RangeSumofBST.py
+++ 0938RangeSumofBST.py
@@ -14,17 +14,30 @@ class Solution(object):
         :type R: int
         :rtype: int
         """
-        def dfs(node):
+        ans=0
+        stack=[root]
+        while stack:
+            node=stack.pop()
             if node:
                 if L<=node.val<=R:
-                    self.ans+=node.val
+                    ans+=node.val
                 if L<node.val:
-                    dfs(node.left)
-                if node.val<R:
-                    dfs(node.right)
-        self.ans=0
-        dfs(root)
-        return self.ans
+                    stack.append(node.left)
+                if R>node.val:
+                    stack.append(node.right)
+        return ans
+        
+        # def dfs(node):
+        #     if node:
+        #         if L<=node.val<=R:
+        #             self.ans+=node.val
+        #         if L<node.val:
+        #             dfs(node.left)
+        #         if node.val<R:
+        #             dfs(node.right)
+        # self.ans=0
+        # dfs(root)
+        # return self.ans
         
         
         # if not root:
