02c7709e99d50271e21023fee0af62eb801efdb2
Caoyuan Li <408271618@qq.com>
2019-09-02 22:10:59 +1000

    recursive, dfs

--- 078Subsets.py
+++ 078Subsets.py
@@ -4,6 +4,28 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+
+        res=[]
+        def dfs(nums, position,current, res):
+            if position==len(nums):
+                res.append(current)
+                return
+            dfs(nums, position+1, current, res)
+            dfs(nums, position+1, current+[nums[position]], res)
+            
+        dfs(nums, 0,[], res)
+        return res
+                
+        
+
+
+
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
         res = []
         self.dfs(nums,0, [],res)
         return res
