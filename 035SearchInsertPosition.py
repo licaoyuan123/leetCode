7ab3c26bb5f7778b770838e102b96519655f819e
Caoyuan Li <408271618@qq.com>
2019-09-05 18:07:03 +1000

    Binary search

--- 035SearchInsertPosition.py
+++ 035SearchInsertPosition.py
@@ -1,3 +1,34 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        #Find the last less than target
+        if not nums:
+            return 0
+        
+        start, end = 0, len(nums)-1
+        while start+1<end:
+            mid = start+(end-start)/2
+            
+            if nums[mid]==target:
+                return mid
+                #end=mid
+            else:
+                if nums[mid]>target:
+                    end=mid
+                else:
+                    start=mid
+        if nums[end]<target:
+            return end+1
+        if nums[start]<target:
+            return start+1
+        else:
+            return start
+
+
 #Shows how to write a correct binary search algorithm
 class Solution(object):
     def searchInsert(self, nums, target):
