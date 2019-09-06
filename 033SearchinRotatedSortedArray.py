2edfd3a0acce457f5e5ef0d380dfe8fd75d5c1b9
Caoyuan Li <408271618@qq.com>
2019-09-06 12:24:07 +1000

    binary search two solutions

--- 033SearchinRotatedSortedArray.py
+++ 033SearchinRotatedSortedArray.py
@@ -46,3 +46,31 @@ class Solution:
             return left
         else:
             return right
+
+ #Soulution2, binary search once, however two situations       
+ class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if not nums:
+            return -1
+        left, right = 0, len(nums)-1
+        while left+1<right:
+            mid = left+(right-left)//2
+            if nums[mid]==target:
+                return mid
+            if nums[left]<nums[mid]:
+                if nums[mid]>target and nums[left]<=target:
+                    right=mid
+                else:
+                    left =mid
+                        
+            else:
+                if nums[mid]<target and target<=nums[right]:
+                    left=mid
+                else:
+                    right=mid
+                
+        if nums[left]==target:
+            return left
+        if nums[right]==target:
+            return right
+        return -1
