ae9c726f9894a48a0f05a7061dbefd3fb0273df5
Caoyuan Li <408271618@qq.com>
2019-09-06 22:04:55 +1000

    binary search which can combine cases together

--- 162FindPeakElement.py
+++ 162FindPeakElement.py
@@ -20,3 +20,28 @@ class Solution(object):
         if nums[start]>nums[end]:
             return start
         return end
+
+    #Combine many case together
+  class Solution(object):
+    def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        #binary search
+        start, end = 0, len(nums)-1
+        while start+1<end:
+            mid=start+(end-start)/2
+            
+            if nums[mid-1]<nums[mid] and nums[mid]>nums[mid+1]:
+                return mid
+            
+            if nums[mid]<nums[mid+1]:
+                start=mid
+            else:
+                # if nums[mid]<nums[mid-1]:
+                end=mid
+                # else:
+        if nums[start]>nums[end]:
+            return start
+        return end
