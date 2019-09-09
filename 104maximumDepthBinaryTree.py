03c89505ba7217cfe71ae3f02c083678a6c22916
Caoyuan Li <408271618@qq.com>
2019-09-09 20:21:04 +1000

    recursive, dfs, bfs

--- 104maximumDepthBinaryTree.py
+++ 104maximumDepthBinaryTree.py
@@ -11,8 +11,53 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
-        #recursive
-        if not root:
-            return 0
-        return max(self.maxDepth(root.left), self.maxDepth(root.right))+1
+        #Solution 4 BFS
+        max_depth=0
+        
+        level=[root] if root else []
+        while level:
+            max_depth+=1
+            queue=[]
+            for node in level:
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            #queue.append()
+            level = queue
+        return max_depth
+        
+        
+        
+        #Solution 3 non recursive
+        
+#         if not root:
+#             return 0
+#         max_depth = 0
+#         stack=[(root, 1)]
+#         while stack:
+#             node, depth=stack.pop()
+            
+#             if node:
+#                 max_depth=max(max_depth, depth)
+#                 stack.append((node.right, depth+1))
+#                 stack.append((node.left, depth+1))
+#         return max_depth
+        
+        
+        
+        #Solution 2 recursive, divide and conque
+        
+#         if not root:
+#             return 0
+        # left= self.maxDepth(root.left)
+        # right=self.maxDepth(root.right)
+        # return 1+max(left, right)
+        
+        #Solution 1 recursive, traverse
+        # if not root:
+        #     return 0
+        # return 1+ max( self.maxDepth(root.left), self.maxDepth(root.right))
+    
+        
         
