fb33df16382278fa1fab60b2afdbdc6562cedd57
Caoyuan Li <408271618@qq.com>
2019-09-11 20:54:54 +1000

    separate inorder traverse

--- 0173BinarySearchTreeIterator.py
+++ 0173BinarySearchTreeIterator.py
@@ -8,6 +8,48 @@
 class BSTIterator:
 
     def __init__(self, root: TreeNode):
+        self.stack=[]
+        self.pushAll(root)
+        
+
+    def next(self) -> int:
+        """
+        @return the next smallest number
+        """
+        node=self.stack.pop()
+        self.pushAll(node.right)
+        return node.val
+        
+
+    def hasNext(self) -> bool:
+        """
+        @return whether we have a next smallest number
+        """
+        return self.stack
+    
+    def pushAll(self, root):
+        while root:
+            self.stack.append(root)
+            root=root.left
+        
+
+
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator(root)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class BSTIterator:
+
+    def __init__(self, root: TreeNode):
         self.index=0
         self.result=[]
         if not root:
