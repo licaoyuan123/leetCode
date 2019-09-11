34811035e18002ddb5d0bb4863e780dd39b5bfd2
Caoyuan Li <408271618@qq.com>
2019-09-11 12:03:48 +1000

    dequeue and two queue BFS

--- 102BinaryTreeLevelOrder.py
+++ 102BinaryTreeLevelOrder.py
@@ -1,42 +1,58 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
-
-class Solution(object):
-    def levelOrder(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[List[int]]
-        """
-        # ans, level=[], [root]
-        # while root and level:
-        #     ans.append([node.val for node in level])
-        #     LRpair  = [(node.left, node.right) for node in level]
-        #     level = [leaf for LR in LRpair for leaf in LR if leaf]
-        # return ans
-        
-        # ans, level=[],[root]
-        # while root and level:
-        #     ans.append([node.val for node in level])
-        #     level =[kid for n in level for kid in (n.left, n.right) if kid]
-        # return ans
-        ans, level = [],[root]
-        while root and level:
-            sub_ans=[]
-            for node in level:
-                sub_ans.append(node.val)
-            ans.append(sub_ans)
-            sub_level=[]
-            for node in level:
-                if node.left:
-                    sub_level.append(node.left)
-                if node.right:
-                    sub_level.append(node.right)
-            level = sub_level
-        return ans
-
-
+from collections import deque
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root:
+            return []
+        level=[root]
+        current=[]
+        for node in level:
+            current.append(node.val)
+            if node.left:
+                level.append(node.left)
+            if node.right:
+                level.append(node.right)
             
+        
+
+        
+    #         queue= deque([root])
+#         result=[]
+#         while queue:
+#             current_level = []
+#             for _ in range(len(queue)):
+#                 node=queue.popleft()
+#                 current_level.append(node.val)
+#                 if node.left:
+#                     queue.append(node.left)
+#                 if node.right:
+#                     queue.append(node.right)
+#             result.append(current_level)
+        
+#         return result
+            
+        
+        
+        
+        # if not root:
+        #     return []
+        # queue = [root]
+        # result=[]
+        # while queue:
+        #     current_level=[]
+        #     second_queue=[]
+        #     for node in queue:
+        #         current_level.append(node.val)
+        #         if node.left:
+        #             second_queue.append(node.left)
+        #         if node.right:
+        #             second_queue.append(node.right)
+        #     result.append(current_level)
+        #     queue=second_queue
+        # return result
+        
