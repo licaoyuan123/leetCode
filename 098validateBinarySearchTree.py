1bf61dfa21c554ce714d75065a5aff328c4c3b9e
Caoyuan Li <408271618@qq.com>
2019-09-11 17:53:13 +1000

    iterative, recursive

--- 098validateBinarySearchTree.py
+++ 098validateBinarySearchTree.py
@@ -1,102 +1,86 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-class Solution(object):
-    def isValidBST(self, root, lo=-float('inf'), hi = float('inf')):
-        """
-        :type root: TreeNode
-        :rtype: bool
-        """
-        if not root:
-            return True
-        if lo<root.val<hi:
-            return self.isValidBST(root.left, lo, min(hi,root.val)) and self.isValidBST(root.right,max(root.val, lo), hi)
-       # return 
-        
-        #My initial solution, however it is out of time limition
-#         if root==None:
-#             return True
-#         if root.left!=None:
-#             if self.ma(root.left)>=root.val:
-#                 return False
-#         if root.right!=None:
-#             if root.val>=self.mi(root.right):
-#                 return False
-        
-#         return self.isValidBST(root.left) and self.isValidBST(root.right)
-#     def ma(self, root):
-#         max_value = root.val
-#         if root.left and self.ma(root.left)>max_value:
-#             max_value = self.ma(root.left)
-#         if root.right and self.ma(root.right)>max_value:
-#             max_value = self.ma(root.right)
-#         return max_value
-            
-#     def mi(self, root):
-#         min_value = root.val
-#         if root.left and self.mi(root.left)<min_value:
-#             min_value = self.mi(root.left)
-#         if root.right and self.mi(root.right)<min_value:
-#             min_value = self.mi(root.right )
-#         return min_value
-
-
-
-# Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
-#Easist way is to travel all the values and judge if it is ordered.
+
 class Solution:
-    def isValidBST(self, root, min1= None, max1 = None):
-        """
-        :type root: TreeNode
-        :rtype: bool
-        """
-        if(not root):
+    def isValidBST(self, root: TreeNode) -> bool:
+        if not root:
             return True
-        if(min1 !=None and root.val<= min1):
-            return False
-        if(max1 !=None and root.val>= max1):
-            return False
-        return self.isValidBST(root.left, min1, root.val) and self.isValidBST(root.right, root.val, max1)
+        stack=[]
+        prev = float("-inf")
+        while root or stack:
+            while root:
+                stack.append(root)
+                root=root.left
+            root=stack.pop()
+            #result.append(root.val)
+            if prev>=root.val:
+                return False
+            prev = root.val
+            root=root.right
+        return True
+        
+        #Third solution
+        # if not root:
+        #     return True
+        # if lo<root.val<hi:
+        #     return self.isValidBST(root.left, lo,  min(hi, root.val)) and self.isValidBST(root.right, max(lo, root.val), hi)
+        # else:
+        #     return False
         
         
-    #     self.prev=None
-    #     return self.helper(root)
-    # def helper(self, root):
-    #     if root is None:
-    #         return True
-    #     if not self.helper(root.left):
-    #         return False
-    #     if self.prev and self.prev.val>=root.val:
-    #         return False
-    #     self.prev=root
-    #     return self.helper(root.right)
         
-#         if not root:
+        #My second solution, inorder traverse the tree
+        #to see if it is ancent
+        #         if not root:
 #             return True
-#         all_value = []
-#         self.travel(root, all_value)
-#         # test= sorted(all_value)
-#         # return test==all_value
-#         for i in range(len(all_value)-1):
-#             if all_value[i]>= all_value[i+1]:
+#         stack=[(root, False)]
+#         result = []
+#         while stack:
+#             node, visited=stack.pop()
+#             if node and not visited:
+#                 stack.append((node.right, False))
+#                 stack.append((node, True))
+#                 stack.append((node.left, False))
+#             if node and visited:
+#                 result.append(node.val)
+        
+#         for i in range(1,len(result)):
+#             if result[i]<=result[i-1]:
 #                 return False
 #         return True
+            
+            
         
-#     def travel(self,root, all_value):
-#         if root is None:
-#             return
-#         self.travel(root.left, all_value)
-#         all_value.append(root.val)
-#         self.travel(root.right, all_value)
+    #My first solution, brute force    
         
+#         self.flag=True
+#         if not root:
+#             return True
         
-        
+#         left= self.isValidBST(root.left)
+#         right=self.isValidBST(root.right)
+
+#         if root.left and self.dfs(root.left)>=root.val:
+#             self.flag=False
+#         if root.right and self.dfs_min(root.right)<=root.val:
+#             self.flag=False
+            
+#         return self.flag and left and right
+#     def dfs(self, root):
+#         #self.max=float("-inf")
+#         if not root:
+#             return float("-inf")
+#         #self.max=max(root.val, self.max)
+#         left = self.dfs(root.left)
+#         right=self.dfs(root.right)
+#         return max(root.val, left, right)
+#     def dfs_min(self, root):
+#         if not root:
+#             return float("inf")
+#         left= self.dfs_min(root.left)
+#         right=self.dfs_min(root.right)
+#         return min(root.val, left, right)
