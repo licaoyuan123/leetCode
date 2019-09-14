4c245349583febed2559b37601ded559cd438892
Caoyuan Li <408271618@qq.com>
2019-09-14 16:04:53 +1000

    DP, greedy

---
+++ 045JumpGameII.py
@@ -0,0 +1,38 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        start, end, farthest, jump = 0, 0, 0, 0
+        while end<len(nums)-1:
+            jump+=1
+            
+            for i in range(start, end+1):
+                if i+nums[i]>farthest:
+                    farthest=i+nums[i]
+            start=end+1
+            end=farthest
+        return jump
+            
+        
+        
+        #farthest=nums[0]
+        #for i in range(len(nums)):
+            
+        
+        
+        # step=[float("inf")]*len(nums)
+        # step[0]=0
+        # for i in range(1, len(nums)):
+        #     for j in range(i):
+        #         if step[j]!=float("inf") and j+nums[j]>=i:
+        #             step[i]=min(step[i], step[j]+1)
+        # return step[-1]
+                    
+        
+        # step= [float("inf")]*len(nums)
+        # step[0]=0
+        # for i in range(len(nums)):
+        #     for j in range(1,nums[i]+1):
+        #         if i+j<len(nums):
+        #             if step[i]+1<step[i+j]:
+        #                 step[i+j] = step[i]+1
+        # return step[-1]
+        
