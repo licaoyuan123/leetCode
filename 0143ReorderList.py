fb87342519501910fcae21136d5032110c286cdd
Caoyuan Li <408271618@qq.com>
2019-09-19 15:03:22 +1000

    two pointers, functions

--- 0143ReorderList.py
+++ 0143ReorderList.py
@@ -1,4 +1,56 @@
 # Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reorderList(self, head: ListNode) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        if not head or not head.next or not head.next.next:
+            return head
+        fast=slow=head
+        tail = None
+        while fast and fast.next :
+            tail=slow
+            fast=fast.next.next
+            slow=slow.next
+        tail.next=None
+        
+
+        second=self.reverse(slow)
+        
+        dummy_node=cur = ListNode(0)
+        dummy_node.next=head
+        first=head
+        current=first
+        self.combine(head, second)
+        return head
+    def reverse(self, head):
+        cur=head
+        pre=None
+        while cur:
+            cur.next, pre, cur = pre, cur, cur.next
+        return pre
+    
+    def combine(self, first, second):
+        while first:
+            n1=first.next
+            n2=second.next
+            first.next = second
+            if not n1:
+                break
+            first=n1
+            second.next=first
+            second=n2
+        
+        
+        
+
+
+# Definition for singly-linked list.
 # class ListNode(object):
 #     def __init__(self, x):
 #         self.val = x
