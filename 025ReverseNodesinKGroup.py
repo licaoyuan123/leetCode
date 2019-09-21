79313f804f26ad71f6fb50a384b3dee949ff2947
Caoyuan Li <408271618@qq.com>
2019-09-21 16:15:52 +1000

    linkedlist

--- 025ReverseNodesinKGroup.py
+++ 025ReverseNodesinKGroup.py
@@ -1,3 +1,37 @@
+#After practice, I got similar or better idea than the previous one
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
+        
+        if not head or not head.next or k==1:
+            return head
+        jump=dummy_node=ListNode(0)
+        dummy_node.next=head
+        while True:
+            start=end=jump.next
+            if not end:
+                return dummy_node.next
+
+            for _ in range(k-1):
+                end=end.next
+                if not end:
+                    return dummy_node.next
+            #reverse start to end
+            pre = end.next
+            cur=start
+            for i in range(k):
+                cur.next, cur, pre= pre, cur.next, cur
+            jump.next= pre
+            jump=start
+
+
+
 #Solution from https://leetcode.com/problems/reverse-nodes-in-k-group/discuss/11491/Succinct-iterative-Python-O(n)-time-O(1)-space
 #How can this guy so smart
 
