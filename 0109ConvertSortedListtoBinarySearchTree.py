9ab0b6810c78f95494d9147e1178074e09d4c86a
Caoyuan Li <408271618@qq.com>
2019-09-21 15:38:01 +1000

    Update 0109ConvertSortedListtoBinarySearchTree.py

--- 0109ConvertSortedListtoBinarySearchTree.py
+++ 0109ConvertSortedListtoBinarySearchTree.py
@@ -1,4 +1,40 @@
 # Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def sortedListToBST(self, head: ListNode) -> TreeNode:
+        if not head:
+            return head
+        if not head.next:
+            return TreeNode(head.val)
+        pre_mid, mid= self.findMiddle(head)
+        pre_mid.next=None
+        root = TreeNode(mid.val)
+        root.left = self.sortedListToBST(head)
+        root.right = self.sortedListToBST(mid.next)
+        return root
+    def findMiddle(self, head):
+        slow=fast=pre_mid=head
+        while fast and fast.next:
+            pre_mid=slow
+            slow=slow.next
+            fast= fast.next.next
+        return pre_mid, slow
+        
+
+
+
+# Definition for singly-linked list.
 # class ListNode(object):
 #     def __init__(self, x):
 #         self.val = x
