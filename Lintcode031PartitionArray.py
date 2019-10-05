53f6ccb94a229425ab7ecd26a0ce9d2eb673a5c5
Caoyuan Li <408271618@qq.com>
2019-10-05 20:15:24 +1000

    Update and rename 031PartitionArray.py to Lintcode031PartitionArray.py

--- Lintcode031PartitionArray.py
+++ Lintcode031PartitionArray.py
@@ -11,16 +11,19 @@ class Solution:
             return 0
         left=0
         right = N-1
-        while left<right:
-            while left<right and nums[left]<k:
+        while left<=right:
+            while left<=right and nums[left]<k:
                 left+=1
             
-            while left<right and nums[right]>=k:
+            while left<=right and nums[right]>=k:
                 right-=1
             
-            if left<right:
+            if left<=right:
                 nums[left], nums[right] = nums[right], nums[left]
-        if nums[right]>=k:
-            return right
-        else:
-            return right+1
+                left+=1
+                right-=1
+        return left
+        # if nums[right]>=k:
+        #     return right
+        # else:
+        #     return right+1
