a65903ca0c95b45e48f85a58e320e00665b546cf
Caoyuan Li <408271618@qq.com>
2019-10-07 14:01:14 +1100

    divide and conque,

--- Lintcode143SortColorsII.py
+++ Lintcode143SortColorsII.py
@@ -1,3 +1,39 @@
+#Solution 1 O(nlogk) divide and conque
+class Solution:
+    """
+    @param colors: A list of integer
+    @param k: An integer
+    @return: nothing
+    """
+    def sortColors2(self, colors, k):
+        # write your code here
+        if not colors:
+            return
+        left=0
+        right = len(colors)-1
+        self.rainbowSort(colors, left, right, 0, k)
+    def rainbowSort(self, colors, left, right, colorFrom, colorTo):
+        if colorFrom==colorTo:
+            return
+        if left>=right:
+            return
+        l=left
+        r=right
+        #cur = left
+        colorMid = (colorFrom+colorTo)//2
+        while l<=r:
+            while l<=r and colors[l]<=colorMid:
+                l+=1
+            while l<=r and colors[r]>colorMid:
+                r-=1
+            if l<=r:
+                colors[l], colors[r] = colors[r], colors[l]
+                l+=1
+                r-=1
+            
+        self.rainbowSort(colors, left, r, colorFrom, colorMid)
+        self.rainbowSort(colors, l, right, colorMid+1, colorTo)
+
 
 #Solution 2 O(nk)
 class Solution:
